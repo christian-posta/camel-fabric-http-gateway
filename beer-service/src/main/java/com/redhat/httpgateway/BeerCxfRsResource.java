51f92e928bd37e2a5d916b36ab077ec8886f873a
Christian Posta <christian.posta@gmail.com>
2014-06-17 14:42:20 -0700

    First crack

---
+++ beer-service/src/main/java/com/redhat/httpgateway/BeerCxfRsResource.java
@@ -0,0 +1,22 @@
+package com.redhat.httpgateway;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+
+/**
+ * Created by ceposta on 5/15/14.
+ */
+@Path("/beer")
+public class BeerCxfRsResource {
+
+    @POST
+    public String testMe(String payload) {
+        return null;
+    }
+
+    @GET
+    public String getMe(String payload) {
+        return null;
+    }
+}
